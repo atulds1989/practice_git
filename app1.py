97c784c4f018e812346322957644774a1e8056c5
atulds1989 <purohit.atuloo7@gmail.com>
2023-12-15 15:22:05 +0530

    on Main branch added e

--- app1.py
+++ app1.py
@@ -1,6 +1,6 @@
 print("this is the first app to practice git")
 
-def addition(a,b,c,d):
-    return a+b+c+d
+def addition(a,b,c,d,e):
+    return a+b+c+d+e
 
-print("result is", addition(3,2,3,4))
+print("result is", addition(3,2,3,4,1))
